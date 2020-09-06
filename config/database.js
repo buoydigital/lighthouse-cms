e602accc9d1eddbd06858505d66920f0a58ce6af
Samantha O'Gorman <samogorm@icloud.com>
2020-09-06 12:21:01 +0100

    Updat mongodb connection vars.

--- config/database.js
+++ config/database.js
@@ -4,16 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        client: 'mongo',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'lighthouse-cms'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        uri:  env('DATABASE_URI', 'mongodb://localhost:27017/lighthouse-cms'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', true),
-        ssl: env('DATABASE_SSL', true),
+        ssl: env('DATABASE_SSL', false),
       },
     },
   },
