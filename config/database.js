becf172922b6cc9eb557b529ed08b09360e81407
Samantha O'Gorman <samogorm@icloud.com>
2020-09-06 10:23:26 +0100

    Update db config to use mysql.

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'lighthouse-cms'),
+        username: env('DATABASE_USERNAME', 'lighthouse'),
+        password: env('DATABASE_PASSWORD', 'root')
       },
       options: {
-        useNullAsDefault: true,
+        debug: true
       },
     },
   },
